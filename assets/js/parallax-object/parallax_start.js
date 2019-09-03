94ea4d8038cfcb302e0c008ac12f6569c34b4d99
Cristi Filip <cristif3@gmail.com>
2019-09-03 15:42:44 +0300

    gradi sion

---
+++ assets/js/parallax-object/parallax_start.js
@@ -0,0 +1,7 @@
+"use strict";
+
+function parallaxStart() {
+    /*------------------ Parallax home background ------------------*/
+    var scene = document.getElementById('scene');
+    var parallaxInstance = new Parallax(scene);
+}
